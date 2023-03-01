16020553198dd4b1f446554a93d2a68a18da5a38
Jon Telleria <telleria.jon@uni.eus>
2023-03-01 13:07:30 +0100

    ahorkatua1.0 amaituta

--- lana.py
+++ lana.py
@@ -1,22 +1,57 @@
 def jonenJokoa():
    
-def pedirNumeroEntero():
+    import random
+
+    def tablerua(hitz_ezkutua, asmatutako_letra):
+    
+        print("Hitz sekretua: ", end="")
+        for letra in hitz_ezkutua:
+            if letra in asmatutako_letra:
+                print(letra, end=" ")
+            else:
+                print("_", end=" ")
+        print()
+
+    def sartuletra():
+        
+        letra = input("Sartu letra bat: ")
+        return letra.lower()
+
+    def jolastu():
+
+        hitza = ["elefantea", "programazioa", "mendizalea", "klasea", "esternokleidomastoideo", "irakaslea"]
+        hitz_ezkutua = random.choice(hitza)
+        asmatutako_letra = set()
+        sahiakerak = 6
+
+        while sahiakerak > 0:
+            tablerua(hitz_ezkutua, asmatutako_letra)
+            letra = sartuletra()
+
+            if letra in asmatutako_letra:
+                print("letra hau sartuta dago sahiatu beste batekin")
+                continue
+
+            asmatutako_letra.add(letra)
+
+            if letra in hitz_ezkutua:
+                print("Oso ondo!!!letra hau hitz honetan dago!")
+            else:
+                sahiakerak -= 1
+                print("Oooooohhh, letra hau ez dago hitz honetan. ", sahiakerak, "sahiakera gelditzen zaizkizu.")
+
+            if set(hitz_ezkutua) == asmatutako_letra:
+                print("IRABAZI DUZU")
+                return
+
+        print("oooooohhh galdu duzu. hitz eskutua:", hitz_ezkutua, "zen")
+
+    jolastu()
  
-    correcto=False
-    num=0
-    while(not correcto):
-        try:
-            num = int(input("Introduce un numero entero: "))
-            correcto=True
-        except ValueError:
-            print('Error, introduce un numero entero')
-     
-    return num
- 
-salir = False
-opcion = 0
- 
-while not salir:
+
+       
+i = 1
+while i == 1:
     
     print("******************************************************** ")
     print("******************************************************** ")
@@ -28,29 +63,26 @@ while not salir:
     print("**          |    |  |||||   |    \\|  |||||||          ** ")
     print("**                                                    ** ")
     print("**- - - - - - - - - - - - - - - - - - - - - - - - - - ** ")
-    print("**   1.                                               ** ")
-    print("**   2.                                               ** ")
+    print("**   1.Wordlen kopia polit bat(Aimar)                 ** ")
+    print("**   2.Ahorkatua                                      ** ")
     print("**   3.                                               ** ")
     print("**   0 para salir                                     ** ")
     print("**                                                    ** ")
     print("******************************************************** ")
     print("******************************************************** ")
-     
-    print ("Elige una opcion")
  
-    opcion = pedirNumeroEntero()
+    opcion = int(input("Aukeratu: "))
  
     if opcion == 1:
-        print ("Opcion 1")
-        break
+        juegoAimar = aimar()
     elif opcion == 2:
-        print ("Opcion 2")
-        break
+        
+        juegoJon = jonenJokoa()
     elif opcion == 3:
         print("Opcion 3")
         break
-    elif opcion == 4:
-        salir = True
+    elif opcion == 0:
+        break
     else:
         print ("Introduce un numero entre 1 y 3")
  
